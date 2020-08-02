24cf84f3547f36157b8009f2a6686c59101e1ed5
leandro.sardi <leandro.sardi@expandedventure.com>
2020-08-02 20:35:03 -0300

    tempora#111

---
+++ cli/recalculation_all.rb
@@ -0,0 +1,86 @@
+#require "blackstack_commons"
+#s = Time.now().to_s
+#puts s
+#t = DateTime.strptime(s, '%Y-%m-%d %H:%M:%S %Z').to_time
+#puts t.to_s
+#exit(0)
+
+require "simple_command_line_parser"
+require "pampa_workers"
+require_relative "../lib/invoicing_payments_processing"
+require_relative './config'
+
+# command line parameters 
+PARSER = BlackStack::SimpleCommandLineParser.new(
+  :description => 'Create a movement about a payment received. If this payment is associated to a PayPal subscription, the command will create a new invoice for the next billing cycle too. This command will also run both recalculations and expiration of credits.', 
+  :configuration => [{
+    :name=>'name', 
+    :mandatory=>false, 
+    :description=>'Name of the worker. Note that the full-name of the worker will be composed with the host-name and the mac-address of this host too.', 
+    :type=>BlackStack::SimpleCommandLineParser::STRING,
+    :default=>DEFAULT_WORKER_NAME,
+  }, {
+    :name=>'division', 
+    :mandatory=>false, 
+    :description=>'Name of the worker. Note that the full-name of the worker will be composed with the host-name and the mac-address of this host too. The invoice cannot be already linked to another subscription.', 
+    :type=>BlackStack::SimpleCommandLineParser::STRING,
+    :default=>DEFAULT_DIVISION_NAME,
+  }]
+)
+
+BlackStack::Pampa::set_division_name(
+  PARSER.value('division')
+)
+
+# connect to DB and require ORM classes
+require 'sequel'
+#puts "Connector Descriptor: #{BlackStack::Pampa::connection_descriptor}"
+DB = BlackStack::Pampa::db_connection
+BlackStack::Pampa::require_db_classes
+BlackStack::InvoicingPaymentsProcessing::require_db_classes
+
+# 
+class MyCLIProcess < BlackStack::MyLocalProcess
+  
+  def process(argv)
+    self.logger.log "Say hello to CLI for IPN manual processing!"
+    
+    self.logger.log "DB:#{DB['SELECT db_name() AS s'].first[:s]}."
+    
+    # process 
+    begin
+			['ssm', 'drx'].each { |service|
+				self.logger.logs "Service #{service}... "
+				BlackStack::Client.where(:delete_time=>nil).all { |c|
+					self.logger.logs "#{c.id.to_guid}.#{c.name}... "
+					c.recalculate(service)
+					self.logger.done
+					# libero recursos
+					GC.start
+					DB.disconnect
+				}
+				self.logger.done
+				# libero recursos
+				GC.start
+				DB.disconnect
+			}			
+    rescue => e
+      self.logger.error(e)
+    end
+
+    # libero recursos
+    self.logger.logs "Release resources... "
+    DB.disconnect
+    GC.start   
+    self.logger.done
+
+    self.logger.logs "Sleep for long time. Click CTRL+C to exit... "
+    sleep(500000)
+    self.logger.done
+
+  end # process  
+end # class
+
+PROCESS = MyCLIProcess.new(PARSER.value('name'), PARSER.value('division'))
+PROCESS.verify_configuration = false # disable this to run any script with the name of this worker-thread, even if worker is configured to run another script
+PROCESS.run()
