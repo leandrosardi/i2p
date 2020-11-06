eb21af6ea3d5dbdb2ac46084fda88a3c233ae1ae
unknown <leandro.sardi@expandedventure.com>
2020-11-06 10:38:04 -0300

    query optimization

--- lib/balance.rb
+++ lib/balance.rb
@@ -10,14 +10,21 @@ module BlackStack
   	end
   	
     def calculate()
-      q = 
-      "select cast(sum(cast(amount as numeric(18,12))) as numeric(18,6)) as amount, sum(credits) as credits " +
-      "from movement with (nolock index(IX_movement__id_client__product_code)) " +
-      "where id_client='#{self.client.id}' " +
-      "and product_code='#{self.product_code}' "
-
-			q += "and create_time < '#{self.up_time.to_time.to_sql}' " if !self.up_time.nil?
-
+      if !self.up_time.nil?
+        q = 
+        "select cast(sum(cast(amount as numeric(18,12))) as numeric(18,6)) as amount, sum(credits) as credits " +
+        "from movement with (nolock) " +
+        "where id_client='#{self.client.id}' " +
+        "and product_code='#{self.product_code}' " +
+  			"and create_time < '#{self.up_time.to_time.to_sql}' "
+      else
+        q = 
+        "select cast(sum(cast(amount as numeric(18,12))) as numeric(18,6)) as amount, sum(credits) as credits " +
+        "from stat_balance x with (nolock) " +
+        "where x.id_client='#{self.client.id}' " +
+        "and x.product_code='#{self.product_code}' " +
+      end
+      
       row = DB[q].first
       self.amount = row[:amount].to_f
       self.credits = row[:credits].to_f
