650506e49d4688e78a5889e4d02da922713a98a9
leandrosardi <leandro.sardi@expandedventure.com>
2023-01-22 20:35:21 -0300

    processes were stopping

--- p/ipn.rb
+++ p/ipn.rb
@@ -13,34 +13,37 @@ BlackStack::Extensions.append :i2p
 l = BlackStack::LocalLogger.new('./ipn.log')
 
 while true
-  # iterate all exports with no start time, and a search.
-  BlackStack::I2P::BufferPayPalNotification.where(:sync_start_time=>nil).all.each do |ipn|
-    l.logs "Processing IPN #{ipn.id}"
-    begin
-        l.logs 'Flag start_time... '
-        ipn.sync_start_time = now
+  begin
+    # iterate all exports with no start time, and a search.
+    BlackStack::I2P::BufferPayPalNotification.where(:sync_start_time=>nil).all.each do |ipn|
+      l.logs "Processing IPN #{ipn.id}"
+      begin
+          l.logs 'Flag start_time... '
+          ipn.sync_start_time = now
+          ipn.save
+          l.done
+
+          l.logs 'Sync IPN... '
+          ipn.process
+          l.done
+
+          l.logs 'Flag end_time... '
+          ipn.sync_end_time = now
+          ipn.save
+          l.done
+      rescue => e
+        l.log "Error: #{e.message}"
+
+        l.logs 'Flag error... '
+        ipn.sync_result = e.message
         ipn.save
         l.done
-
-        l.logs 'Sync IPN... '
-        ipn.process
-        l.done
-
-        l.logs 'Flag end_time... '
-        ipn.sync_end_time = now
-        ipn.save
-        l.done
-    rescue => e
-      l.log "Error: #{e.message}"
-
-      l.logs 'Flag error... '
-      ipn.sync_result = e.message
-      ipn.save
+      end
       l.done
-    end
-    l.done
-
-  end # exports.each 
+    end # exports.each 
+  rescue => e
+    l.logf "Error: #{e.message}"
+  end 
 
   l.logs 'Sleeping... '
   sleep(10)
