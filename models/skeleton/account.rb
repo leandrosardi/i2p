13c22b6206fc6de700c8e33582ecd535bbdce1b8
leandrosardi <leandro.sardi@expandedventure.com>
2023-02-03 18:58:23 -0300

    leandrosardi/mysaas#138

--- models/skeleton/account.rb
+++ models/skeleton/account.rb
@@ -6,6 +6,10 @@ module BlackStack
       		one_to_many :subscriptions, :class=>:'BlackStack::I2P::Subscription', :key=>:id_account
       		#one_to_many :customplans, :class=>:'BlackStack::I2P::CustomPlan', :key=>:id_account
 
+			def premium?
+				self.subscriptions.size > 0
+			end
+
 	  		def hasBillingAddress?
 				false
 				# TODO: Code Me! 
