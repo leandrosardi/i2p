ec3cf46362a5f3242a973e309d13fbfdd03e5373
leandrosardi <leandro.sardi@expandedventure.com>
2023-02-14 16:15:17 -0300

    blocking glitch on IPN processing

--- models/skeleton/movement.rb
+++ models/skeleton/movement.rb
@@ -17,7 +17,7 @@ module BlackStack
         super
         # if it is payment and is not premium, update the account as premium
         if self.type == MOVEMENT_TYPE_ADD_PAYMENT && !self.account.premium 
-          DB.execute("UPDATE \"account\" SET premium=TRUE WHERE id='#{self.id_accout}'")
+          DB.execute("UPDATE \"account\" SET premium=TRUE WHERE id='#{self.id_account}'")
         end
       end # after_create
 
@@ -25,7 +25,7 @@ module BlackStack
         super
         # if it is payment and is not premium, update the account as premium
         if self.type == MOVEMENT_TYPE_ADD_PAYMENT && !self.account.premium 
-          DB.execute("UPDATE \"account\" SET premium=TRUE WHERE id='#{self.id_accout}'")
+          DB.execute("UPDATE \"account\" SET premium=TRUE WHERE id='#{self.id_account}'")
         end
       end # after_update
 
