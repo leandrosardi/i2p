d42944b379532bd09b384751ec12433c4f2cd795
leandrosardi <leandro.sardi@expandedventure.com>
2023-04-21 18:14:12 -0300

    leandrosardi/cs#72

---
+++ p/expire.rb
@@ -0,0 +1,56 @@
+# Reference: https://github.com/leandrosardi/cs/issues/69
+
+# load gem and connect database
+require 'mysaas'
+require 'lib/stubs'
+require 'config'
+require 'version'
+DB = BlackStack::CRDB::connect
+require 'lib/skeletons'
+require 'extensions/i2p/lib/skeletons'
+
+# add required extensions
+BlackStack::Extensions.append :i2p
+
+l = BlackStack::LocalLogger.new('./expire.log')
+
+# process pending IPNs
+while true
+    begin
+        # all expired payments or bonuses from accounts with 0 active subscriptions
+        q = "
+        select a.id as id_account, a.name as account_name, u.email, m.id as id_movement, m.type, m.service_code, m.credits, m.create_time, m.expiration_time, m.expiration_on_next_payment, count(s.id) as active_subscriptions 
+        from movement m 
+        join \"account\" a on a.id=m.id_account
+        left join \"user\" u on u.id=a.id_user_to_contact  
+        left join \"subscription\" s on (a.id=s.id_account and coalesce(s.active,false)=true)
+        where m.expiration_end_time is null
+        and m.expiration_time < CAST('#{now()}' as TIMESTAMP)
+        and m.type in (#{BlackStack::I2P::Movement::MOVEMENT_TYPE_ADD_PAYMENT}, #{BlackStack::I2P::Movement::MOVEMENT_TYPE_ADD_BONUS})
+--and a.name='ERTC Rapid Rebate'
+        group by a.id, a.name, u.email, m.id, m.type, m.service_code, m.credits, m.create_time, m.expiration_time, m.expiration_on_next_payment
+        having count(s.id) = 0
+        order by a.name, a.id, m.create_time
+        "
+        DB[q].all { |row|
+            l.logs "#{row[:account_name]}.#{row[:id_movement]}... "
+            m = BlackStack::I2P::Movement.where(:id=>row[:id_movement]).first
+            b = BlackStack::I2P::Account.where(:id=>m.id_account).first
+            # update balance
+            balance = BlackStack::I2P::Balance.new(b.id, m.service_code)
+            balance.calculate
+            balance.save
+            # expire this movement
+            m.expire
+            l.done
+        }
+
+    rescue => e
+        l.logf "Error: #{e.message}"
+    end 
+
+    l.logs 'Sleeping... '
+    sleep(24*60*10) # iterate every 24 hours
+    l.done
+
+end # while true
