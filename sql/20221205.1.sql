00898e3582a9f4fcddad6cfbaf1f3fa7252090ab
Hamad Rana <hamadrana23@gmail.com>
2023-05-03 16:04:00 +0500

    cs#64

--- sql/20221205.1.sql
+++ sql/20221205.1.sql
@@ -1,2 +1,2 @@
-alter table buffer_paypal_notification drop column if exists sync_reservation_id;
+-- alter table buffer_paypal_notification drop column if exists sync_reservation_id;
 alter table buffer_paypal_notification add column if not exists sync_reservation_id varchar(500) null;
